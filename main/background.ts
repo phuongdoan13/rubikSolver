4cfa2713e478dad024467c004a4720112806c1d2
phuongdoan13 <mphuongdoant@gmail.com>
2023-11-25 09:40:40 +1100

    Merge: solved conflict

--- main/background.ts
+++ main/background.ts
@@ -6,45 +6,35 @@ import { createWindow } from "./helpers";
 const isProd = process.env.NODE_ENV === "production";
 
 if (isProd) {
-	serve({ directory: "app" });
+  serve({ directory: "app" });
 } else {
-	app.setPath("userData", `${app.getPath("userData")} (development)`);
+  app.setPath("userData", `${app.getPath("userData")} (development)`);
 }
 
 (async () => {
-	await app.whenReady();
+  await app.whenReady();
 
-	const mainWindow = createWindow("main", {
-		width: 1000,
-		height: 600,
-		webPreferences: {
-			preload: path.join(__dirname, "preload.js"),
-		},
-	});
+  const mainWindow = createWindow("main", {
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-<<<<<<< HEAD
   if (isProd) {
-    await mainWindow.loadURL('app://./home')
+    await mainWindow.loadURL("app://./home");
   } else {
-    const port = process.argv[2]
-    await mainWindow.loadURL(`http://localhost:${port}/home`)
+    const port = process.argv[2];
+    await mainWindow.loadURL(`http://localhost:${port}/home`);
     // mainWindow.webContents.openDevTools()
   }
-})()
-=======
-	if (isProd) {
-		await mainWindow.loadURL("app://./home");
-	} else {
-		const port = process.argv[2];
-		await mainWindow.loadURL(`http://localhost:${port}/home`);
-	}
 })();
->>>>>>> 3c96659203c24bd4f6b03f7d1fdf96e1f48d99d7
 
 app.on("window-all-closed", () => {
-	app.quit();
+  app.quit();
 });
 
 ipcMain.on("message", async (event, arg) => {
-	event.reply("message", `${arg} World!`);
+  event.reply("message", `${arg} World!`);
 });
